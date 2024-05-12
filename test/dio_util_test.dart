56529151adfabbc4a971c1d94c326a45558c0f55
创圣大天使Evol <296662402@qq.com>
2024-05-12 17:04:13 +0800

    dio_util_test.dart :: test with dummyjson

---
+++ test/dio_util_test.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_ce_picgo/utils/dio_util.dart';
+import 'package:flutter_ce_picgo/utils/logger_util.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('dio_util tests', () async {
+    await initLogger();
+    await setupDio();
+    var response = await dio.get('https://dummyjson.com/products/1');
+    expect(response.statusCode, equals(200));
+    expect(response.data, isNotNull);
+    if (response.statusCode == 200) {
+      logger.w(response.data);
+    }
+  });
+}
+
+class Product {
+  int id;
+  String title;
+  String description;
+  int price;
+  double discountPercentage;
+  double rating;
+  int stock;
+  String brand;
+  String category;
+  String thumbnail;
+  List<String> images;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.discountPercentage,
+    required this.rating,
+    required this.stock,
+    required this.brand,
+    required this.category,
+    required this.thumbnail,
+    required this.images,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+    id: json["id"],
+    title: json["title"],
+    description: json["description"],
+    price: json["price"],
+    discountPercentage: json["discountPercentage"]?.toDouble(),
+    rating: json["rating"]?.toDouble(),
+    stock: json["stock"],
+    brand: json["brand"],
+    category: json["category"],
+    thumbnail: json["thumbnail"],
+    images: List<String>.from(json["images"].map((x) => x)),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "description": description,
+    "price": price,
+    "discountPercentage": discountPercentage,
+    "rating": rating,
+    "stock": stock,
+    "brand": brand,
+    "category": category,
+    "thumbnail": thumbnail,
+    "images": List<dynamic>.from(images.map((x) => x)),
+  };
+}
