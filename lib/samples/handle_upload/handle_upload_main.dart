54b843cec287549149fd6ec83db7feae9c2b2d66
创圣大天使Evol <296662402@qq.com>
2024-05-13 23:21:05 +0800

    handle_upload_main :: configure appWindow & DbProvider

--- lib/samples/handle_upload/handle_upload_main.dart
+++ lib/samples/handle_upload/handle_upload_main.dart
@@ -1,14 +1,52 @@
 import 'dart:io';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ce_picgo/models/uploaded_image.dart';
 
+import '../../database/db_interface.dart';
+import '../../utils/logger_util.dart';
+import '../../utils/shared_preferences_ext.dart';
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initLogger();
+
+  logger.i('configure the usage of SharedPreferences...');
+  await useSharedPreferences();
+
+  logger.i('configure the database provider...');
+  dbProvider = DbInterface();
+  // await dbProvider.init();
+
+  if (!kIsWeb && Platform.isWindows) {
+    appWindow.size = const Size(375, 667);
+    logger.i('Platform is windows, configure the appWindow successfully...');
+  } else {
+    logger.i(
+        'Platform is not windows, unnecessary to configure the appWindow...');
+  }
+
   runApp(const MaterialApp(
     home: MyApp(),
   ));
+
+  if (!kIsWeb && Platform.isWindows) {
+    appWindow.show();
+    doWhenWindowReady(() {
+      final win = appWindow;
+      const initialSize = Size(375, 667);
+      win.minSize = initialSize;
+      win.maxSize = initialSize;
+      win.size = initialSize;
+      win.alignment = Alignment.center;
+      win.title = "Flutter PicGo";
+      win.show();
+    });
+  }
 }
 
 class MyApp extends StatefulWidget {
