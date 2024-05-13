e1211efa84858298cd044cc509266850161f426c
创圣大天使Evol <296662402@qq.com>
2024-05-13 18:53:02 +0800

    github_repo_api PUT test

--- test/github_api_test.dart
+++ test/github_api_test.dart
@@ -2,51 +2,66 @@ import 'dart:io';
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_ce_picgo/models/github_content.dart';
 import 'package:flutter_ce_picgo/utils/logger_util.dart';
-
-const githubToken = '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';
-const repo = 'CrossEvol/picgo-repo';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
-  await initLogger();
+  const githubToken = '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';
+  const repo = 'CrossEvol/picgo-repo';
+  var path = '';
+  var sha = '';
 
-  Dio dio = Dio();
+  test('time', () {
+    print(DateTime.now().toString());
+    print(DateTime.now().millisecondsSinceEpoch);
+    print(DateTime.now().millisecond);
+    print(DateTime.now().microsecondsSinceEpoch);
+    print(DateTime.now().microsecond);
+  });
 
-  // Set headers
-  dio.options.headers['Accept'] = 'application/vnd.github+json';
-  dio.options.headers['Authorization'] = 'Bearer $githubToken';
-  dio.options.headers['X-GitHub-Api-Version'] = '2022-11-28';
-  dio.options.headers['Content-Type'] = 'application/json';
-  dio.interceptors.add(LogInterceptor(
-      requestBody: false, responseBody: true, logPrint: (o) => logger.w(o)));
+  test('github repo put request test', () async {
+    await initLogger();
 
-  var imageFile = File(
-      r'D:\androidStudio\AndroidStudioProjects\flutter_ce_picgo\assets\images\icon_empty_album.png');
-  var fileData = await imageFile.readAsBytes();
+    Dio dio = Dio();
 
-  // Set request body
-  Map<String, dynamic> requestBody = {
-    'message': 'my commit message',
-    'content': base64Encode(fileData),
-  };
+    // Set headers
+    dio.options.headers['Accept'] = 'application/vnd.github+json';
+    dio.options.headers['Authorization'] = 'Bearer $githubToken';
+    dio.options.headers['X-GitHub-Api-Version'] = '2022-11-28';
+    dio.options.headers['Content-Type'] = 'application/json';
+    dio.interceptors.add(LogInterceptor(
+        requestBody: false, responseBody: true, logPrint: (o) => logger.w(o)));
 
-  // Perform PUT request
-  try {
-    Response response = await dio.put(
-      'https://api.github.com/repos/$repo/contents/${DateTime.now().millisecond}.jpg',
-      data: requestBody,
-      options: Options(contentType: Headers.jsonContentType),
-    );
-    print(response.data);
-  } catch (e) {
-    print('Error: $e');
-  }
-}
+    var imageFile = File(
+        r'D:\androidStudio\AndroidStudioProjects\flutter_ce_picgo\assets\images\icon_empty_album.png');
+    var fileData = await imageFile.readAsBytes();
 
-Future<FormData> createFormData() async {
-  return FormData.fromMap({
-    'file': await MultipartFile.fromFile(
-        r'D:\androidStudio\AndroidStudioProjects\flutter_ce_picgo\assets\images\icon_empty_album.png',
-        filename: 'empty.jpg'),
+    // Set request body
+    Map<String, dynamic> requestBody = {
+      'message': 'my commit message',
+      'content': base64Encode(fileData),
+    };
+
+    path = '${DateTime.now().millisecondsSinceEpoch}.jpg';
+    // Perform PUT request
+    try {
+      Response response = await dio.put(
+        'https://api.github.com/repos/$repo/contents/$path',
+        data: requestBody,
+        options: Options(contentType: Headers.jsonContentType),
+      );
+      expect(response.statusCode, equals(201));
+      var jsonData = response.data;
+      var githubContent = GithubContent.fromJson(jsonData['content']);
+      logger.i(githubContent.toJson());
+      sha = githubContent.sha;
+    } catch (e) {
+      logger.e(e);
+    }
+  });
+
+  test('sha', () {
+    print(sha);
   });
 }
